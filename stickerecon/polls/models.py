47672b86792f4054213b79bd63276163ade30fcc
Evan Farrell <evan.farrell0@gmail.com>
2017-12-02 20:17:23 -0500

    add view

--- stickerecon/polls/models.py
+++ stickerecon/polls/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Sticker(models.Model):
+    name = models.CharField(max_length=30)
+    votes =  models.IntegerField(null=False)
+    image = models.FileField()
--- stickerecon/polls/views.py
+++ stickerecon/polls/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
+
+def home(request):
+    stickers = Sticker.objects.all()
+
+    context = {
+        "stickers": stickers,
+    }
+
+    return render(request, "index.html", context)
 
 # Create your views here.
